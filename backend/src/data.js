4d22f4e37581418cc7b3d4f1bb955840e97d1021
Dharmendra Yadav <you@example.com>
2023-01-04 16:18:00 +0530

    first commit

---
+++ backend/src/data.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://Dharmendra:dkyadav123@cluster0.kq9bu.mongodb.net/instagram-clone",
+    Jwt_secret: "dharmendrakumaryadav@123321123"
+
+};
---
+++ frontend/src/components/Profie.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import PostDetail from "./PostDetail";
+import "../CSS/Profile.css";
+
+export default function Profie() {
+  const [pic, setPic] = useState([]);
+  const [show, setShow] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  const toggleDetails = (posts) => {
+    if (show) {
+      setShow(false);
+    } else {
+      setShow(true);
+      setPosts(posts);
+    }
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/myposts", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setPic(result);
+        console.log(pic);
+      });
+  }, []);
+
+  return (
+    <div className="profile">
+      {/* Profile frame */}
+      <div className="profile-frame">
+        {/* profile-pic */}
+        <div className="profile-pic">
+          <img
+            src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        {/* profile-data */}
+        <div className="pofile-data">
+          <h1>{JSON.parse(localStorage.getItem("user")).name}</h1>
+          <div className="profile-info" style={{ display: "flex" }}>
+            <p>50 posts</p>
+            <p>89 followers</p>
+            <p>55 following</p>
+          </div>
+        </div>
+      </div>
+      <hr
+        style={{
+          width: "90%",
+
+          opacity: "0.8",
+          margin: "25px auto",
+        }}
+      />
+      {/* Gallery */}
+      <div className="gallery">
+        {pic.map((pics) => {
+          return (
+            <img
+              key={pics._id}
+              src={pics.photo}
+              onClick={() => {
+                toggleDetails(pics);
+              }}
+              className="item"
+            ></img>
+          );
+        })}
+      </div>
+      {show && <PostDetail item={posts} toggleDetails={toggleDetails} />}
+    </div>
+  );
+}
